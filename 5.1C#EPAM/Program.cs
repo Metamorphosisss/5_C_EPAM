eb5d8fc7e66e4f71afc9dbf0291692c390c97876
Ivan <mr.komar991@gmail.com>
2024-04-15 18:09:09 +0300

    first commit

---
+++ 5.1C#EPAM/Program.cs
@@ -0,0 +1,31 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("length please:");
+        int length = Convert.ToInt32(Console.ReadLine());
+        int[] array = new int[length];
+
+        Console.WriteLine("your numbers please:");
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = Convert.ToInt32(Console.ReadLine());
+        }
+
+        Console.WriteLine(MaxNumberInArray(array));
+    }
+    static int MaxNumberInArray(int[] array)
+    {
+        int maxnumber = array[0];
+
+        for (int i = 1; i < array.Length; i++)
+        {
+            if (array[i] > maxnumber)
+            {
+                maxnumber = array[i];
+            }
+        }
+
+        return maxnumber;
+    }
+}
---
+++ 5C#EPAM/Program.cs
@@ -0,0 +1,15 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("enter two numbers: ");
+        int first_number = Convert.ToInt32(Console.ReadLine());
+        int seconnd_number = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("average number -> " + CalculateAverageNumber(first_number, seconnd_number));
+    }
+    static double CalculateAverageNumber(double x, double y)
+    {
+        return (x + y) / 2;
+    }
+}
